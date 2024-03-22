ce66f0311c94f00c747b6be7f4bf8cccecde351c
dev_hanif <152361547+hanifabbas786@users.noreply.github.com>
2024-03-22 23:52:51 +0500

    Initial commit

---
+++ add two floating numbers and swhos in screen sum.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main;
+{
+    float num1,num2,result;
+    num1=10.90;
+    num2=10.10;
+    result=num1+num2;
+    cout<<num1+num2<<"="<<endl;
+    return 0;
+}
